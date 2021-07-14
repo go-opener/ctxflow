c9cecd8e41ed22e32d30261c16043de4668cad26
sioomy <sioomy@MacBook-Pro-3.local>
2021-07-14 13:19:43 +0800

    add adapter

---
+++ lib/adapter/demo_adapter.go
@@ -0,0 +1,52 @@
+package adapter
+//
+// import (
+//     "encoding/json"
+//     "github.com/go-opener/ctxflow/layer"
+//     "xxxxx.com/pkg/golib/v2/base"
+//     "github.com/mitchellh/mapstructure"
+// )
+//
+// type ApiClientAdapter struct {
+//     layer.Flow
+//     *base.ApiClient
+// }
+//
+// func (entity *ApiClientAdapter) PreUse(args ...interface{}) {
+//     entity.ApiClient = args[0].(*base.ApiClient)
+//     entity.Flow.PreUse(args...)
+// }
+//
+// func (entity *ApiClientAdapter) HttpPost(path string, opts base.HttpRequestOptions) (*base.ApiResult, error) {
+//     return entity.ApiClient.HttpPost(entity.GetContext(),path,opts)
+// }
+//
+// func (entity *ApiClientAdapter) HttpGet(path string, opts base.HttpRequestOptions) (*base.ApiResult, error) {
+//     return entity.ApiClient.HttpGet(entity.GetContext(),path,opts)
+// }
+//
+// func (entity *ApiClientAdapter) DecodeResponse(res *base.ApiResult, output interface{}) (errno int, err error) {
+//     var r base.DefaultRender
+//     if err = json.Unmarshal(res.Response, &r); err != nil {
+//         entity.Errorf("http response decode err, err: %s", res.Response)
+//         return errno, err
+//     }
+//
+//     errno = r.ErrNo
+//     if r.ErrNo != 0 {
+//         entity.Errorf( "http response code: %d", r.ErrNo)
+//         return errno, err
+//     }
+//
+//     if _, ok := r.Data.(map[string]interface{}); !ok {
+//         return errno, nil
+//     }
+//
+//     if err := mapstructure.Decode(r.Data, &output); err != nil {
+//         entity.Warnf( "api call data decode error: %s", err.Error())
+//         return errno, err
+//     }
+//
+//     return errno, nil
+// }
+//
